ba3970c0d2376f562502190bc5ee215aff37d818
zzz <zzz@zzz.com>
2019-04-11 01:15:17 +0300

    options

--- options.js
+++ options.js
@@ -1,31 +1,57 @@
 const options = new Options();
 options.onPersisted = function () {
-    console.log("options saved", options);
+    // console.log("options saved", options);
     qs('.notify').className += ' elementFadeOut';
 };
 
 const ogm = new OptionsGuiMapping();
 
 function OptionsGuiMapping() {
-    this.checkboxByName = {};
+    this.controlByName = {};
     this.init = function (options) {
-        const checkboxes = qsa('input[type="checkbox"]');
-        for (let i = 0; i < checkboxes.length; i++) {
-            const cbx = checkboxes[i];
-            if (cbx.dataset.storageName) {
-                this.checkboxByName[cbx.dataset.storageName] = cbx;
-                cbx.addEventListener('click', function () {
-                    options.save(cbx.dataset.storageName, cbx.checked);
-                });
+        {
+            const controls = qsa('input[type="checkbox"]');
+            for (let i = 0; i < controls.length; i++) {
+                const cbx = controls[i];
+                if (cbx.dataset.storageName) {
+                    this.controlByName[cbx.dataset.storageName] = {
+                        type: 'checkbox',
+                        control: cbx,
+                    };
+                    cbx.addEventListener('click', function () {
+                        options.save(cbx.dataset.storageName, cbx.checked);
+                    });
+                }
+            }
+        }
+        {
+            const $controls = qsa('select');
+            for (let i = 0; i < $controls.length; i++) {
+                const $control = $controls[i];
+                if ($control.dataset.storageName) {
+                    this.controlByName[$control.dataset.storageName] = {
+                        type: 'select',
+                        control: $control,
+                    };
+                    $control.addEventListener('change', function () {
+                        options.save($control.dataset.storageName, $control.value);
+                    });
+                }
             }
         }
     };
     this.gotOptions = function (options) {
-        for (const name in this.checkboxByName) {
-            if (this.checkboxByName.hasOwnProperty(name)) {
-                const $cbx = this.checkboxByName[name];
-                if ($cbx) {
-                    $cbx.checked = options[name];
+        for (const name in this.controlByName) {
+            if (this.controlByName.hasOwnProperty(name)) {
+                const data = this.controlByName[name];
+                if (data) {
+                    if (data.type === 'checkbox') {
+                        const $cbx = data.control;
+                        $cbx.checked = options[name];
+                    } else if (data.type === 'select') {
+                        const $cbx = data.control;
+                        $cbx.value = options[name];
+                    }
                 }
             }
         }
