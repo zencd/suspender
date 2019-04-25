8ea97e577ad713a1ad771c6190a223aa546904f1
zzz <zzz@zzz.com>
2019-04-25 10:00:10 +0300

    wip

--- park/park.js
+++ park/park.js
@@ -3,34 +3,34 @@
 
     console.log("park.js started at", new Date() - startTime);
 
-    function generateAndSetGradient() {
-        function easeInOutQuad(t) {
-            return t < .5 ? 2 * t * t : -1 + (4 - 2 * t) * t
-        }
+    function easeInOutQuad(t) {
+        return t < .5 ? 2 * t * t : -1 + (4 - 2 * t) * t
+    }
 
-        const gradientStartColor = B.split(',').map((s) => parseInt(s));
-        // console.log("gradientStartColor", gradientStartColor);
+    function generateAndSetGradient() {
+        const bg = B.split(',').map((s) => parseInt(s)); // pre-calculated darken bg color
 
         const gradientTopPadding = 50;
         const gradientHeight = 500;
         const gradientSteps = 10;
-        const startColor = [gradientStartColor[0], gradientStartColor[1], gradientStartColor[2], 1.0];
-        const endColor = [gradientStartColor[0], gradientStartColor[1], gradientStartColor[2], 0.1];
+        const opacity = [1.0, 0.1];
+        const startColor = [bg[0], bg[1], bg[2], opacity[0]];
+        const endColor = [bg[0], bg[1], bg[2], opacity[1]];
         const ranges = [
             (endColor[0] - startColor[0]),
             (endColor[1] - startColor[1]),
             (endColor[2] - startColor[2]),
-            (endColor[3] - startColor[3]),
+            (endColor[3] - startColor[3])
         ];
 
         const stops = [];
         for (let i = 0; i <= gradientSteps; i++) {
-            const x = i / gradientSteps;
-            const y = easeInOutQuad(x);
+            const x = i / gradientSteps; // [0..1]
+            const y = easeInOutQuad(x); // [0..1]
             const r = Math.round(startColor[0] + ranges[0] * y);
             const g = Math.round(startColor[1] + ranges[1] * y);
             const b = Math.round(startColor[2] + ranges[2] * y);
-            const a = (startColor[3] + ranges[3] * y).toFixed(4);
+            const a = (startColor[3] + ranges[3] * y).toFixed(3);
             const pos = (gradientTopPadding + gradientHeight * x) + "px";
             const s = "rgba(" + r + "," + g + "," + b + "," + a + ") " + pos;
             // console.log("s", s);
